cf9039aedeb0ae0ba105fc6b2711acaf5d943972
Marat <markosian.m19@gmail.com>
2022-12-04 15:54:11 +0200

    Login and roulette view controller
    Added:
    -LoginVC(email)
    -Roulette spining animation(SwiftUI)

---
+++ Casino/Casino/LogInVC.swift
@@ -0,0 +1,107 @@
+//
+//  LogInVC.swift
+//  Casino
+//
+//  Created by Марат Маркосян on 02.12.2022.
+//
+
+import UIKit
+import FirebaseAuth
+import GoogleSignIn
+
+class LogInVC: UIViewController {
+
+    private lazy var emailTxt = CustomTxtField()
+    private lazy var passwordTxt = CustomTxtField()
+    private lazy var loginBtn = UIButton()
+    
+    override func loadView() {
+        super.loadView()
+        
+        setUpSubviews()
+        setUpAutoLayout()
+    }
+    
+    private func setUpSubviews() {
+        view.backgroundColor = .white
+        
+        view.addSubview(emailTxt)
+        view.addSubview(passwordTxt)
+        view.addSubview(loginBtn)
+        
+        emailTxt.translatesAutoresizingMaskIntoConstraints = false
+        passwordTxt.translatesAutoresizingMaskIntoConstraints = false
+        loginBtn.translatesAutoresizingMaskIntoConstraints = false
+                
+        loginBtn.titleLabel?.font = UIFont(name: "Avenir-Heavy", size: 25)
+        loginBtn.setTitleColor(.black, for: .normal)
+        loginBtn.setTitle("Log In", for: .normal)
+        loginBtn.addTarget(self, action: #selector(logIn), for: .touchUpInside)
+
+        emailTxt.placeholder = "e-mail"
+        passwordTxt.placeholder = "password"
+        
+        emailTxt.textContentType = .emailAddress
+        passwordTxt.textContentType = .password
+        emailTxt.keyboardType = .emailAddress
+        passwordTxt.isSecureTextEntry = true
+        
+    }
+    
+    
+    private func setUpAutoLayout() {
+        NSLayoutConstraint.activate([
+            emailTxt.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            emailTxt.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant:  10),
+            emailTxt.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            emailTxt.heightAnchor.constraint(equalToConstant: 40),
+            
+            passwordTxt.topAnchor.constraint(equalTo: emailTxt.bottomAnchor, constant: 30),
+            passwordTxt.leadingAnchor.constraint(equalTo: emailTxt.leadingAnchor),
+            passwordTxt.trailingAnchor.constraint(equalTo: emailTxt.trailingAnchor),
+            passwordTxt.heightAnchor.constraint(equalToConstant: 40),
+            
+            loginBtn.topAnchor.constraint(equalTo: passwordTxt.bottomAnchor, constant: 50),
+            loginBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            
+        ])
+    }
+
+}
+
+extension LogInVC {
+    
+    func login(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+            if let result = authResult {
+                self!.dismiss(animated: true)
+            } else if let error = error {
+                self?.showError(descr: error.localizedDescription)
+            }
+        }
+    }
+    
+    
+    @objc func logIn() {
+        if let email = emailTxt.text, let password = passwordTxt.text {
+            if password.count > 8 {
+                login(email: email, password: password)
+            } else {
+                showError(descr: "Password should contain more then 8 characters")
+            }
+        }
+
+    }
+    
+    func showError(descr: String) {
+        let alert = UIAlertController(title: "Error", message: descr, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+        }
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+        
+    }
+    
+}
--- Casino/Casino/RouletteVC.swift
+++ Casino/Casino/RouletteVC.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import SimpleRoulette
+import SwiftUI
 
 class RouletteVC: UIViewController {
     
+    private lazy var rolSwiftUI = ContentView()
+    private lazy var rouletteSpin = UIHostingController(rootView: rolSwiftUI)
+    
+    private lazy var roulView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +26,25 @@ class RouletteVC: UIViewController {
     private func setUpSubviews() {
         view.backgroundColor = .white
         
+        view.addSubview(roulView)
+        roulView.translatesAutoresizingMaskIntoConstraints = false
+        rouletteSpin.view.translatesAutoresizingMaskIntoConstraints = false
+        addChild(rouletteSpin)
+        rouletteSpin.view.frame = roulView.frame
+        roulView.addSubview(rouletteSpin.view)
+        rouletteSpin.didMove(toParent: self)
+        
     }
 
     private func setUpAutoLayout() {
-        
+        NSLayoutConstraint.activate([
+            roulView.heightAnchor.constraint(equalToConstant: 200),
+            roulView.widthAnchor.constraint(equalToConstant: 200),
+            roulView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            roulView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            rouletteSpin.view.widthAnchor.constraint(equalToConstant: 200),
+            rouletteSpin.view.heightAnchor.constraint(equalToConstant: 200)
+        ])
     }
 
 }
